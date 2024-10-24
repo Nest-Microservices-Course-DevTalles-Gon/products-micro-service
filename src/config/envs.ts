03b0bff74eac6662d259f7d3e97540192b663acb
Gonzalo Buasso <gonzalobuasso@gmail.com>
2024-10-24 21:14:48 +0200

    update envs.ts

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import * as joi from 'joi';
 
-interface Envs {
+interface EnvsVars {
     PORT: number;
     DATABASE_URL: string
 
@@ -24,7 +24,7 @@ if (error) {
     throw new Error(`Config validation error: ${error.message}`);
 }
 
-const envVars = value;
+const envVars: EnvsVars = value;
 
 export const envs = {
     port: envVars.PORT,
